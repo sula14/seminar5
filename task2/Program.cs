c0b7042601a0dedb5a54e1b8494bf3e296314747
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-25 01:45:08 +0300

    Wrote code for second task

---
+++ task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int result = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (i % 2 == 1)
+    {
+        result = result + numbers[i];
+    }
+}
+
+Console.WriteLine($"Cумма элементов стоящих на нечетных позициях = {result}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1,100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
